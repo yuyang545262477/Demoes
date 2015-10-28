9123c4fe5b4ff109c9e99367c923bc621705ed25
David_Yu <yuyang545262477@gmail.com>
2015-10-29 01:12:16 +0800

    2015.10.28;

--- Demo2/transition/js/main.js
+++ Demo2/transition/js/main.js
@@ -1,8 +1,17 @@
+/* global jQuery */
 (function ($) {
 	//定义私有方法
-	var PrivateFun = function () {
-
-	};
+	var _prefix = (function (temp) {
+		//存放浏览器前缀
+		var aPrefix = ["webkit", "Moz", "o", "ms"],
+			props = "";
+		for (var i in aPrefix) {
+			props = aPrefix[i] + "Transition";
+			if (temp.style[props] !== undefined) {
+				return "-" + aPrefix[i] + "-";
+			}
+		}
+	})(PageSwitch);
 	var PageSwitch = (function () {
 		function PageSwitch(element, options) {
 			this.settings = $.extend(true, $.fun.PageSwitch.defaults, options || {});
@@ -14,16 +23,17 @@
 			// 实现: 初始化dom结构,布局,分页及绑定时间
 			init: function () {
 				var me = this;
-				console.log(me);
 				//dom 初始化
 				me.selectors = me.settings.selectors;
-				me.sections = me.selectors.sections;
-				me.section = me.selectors.section;
+				me.sections = me.element.find(me.selectors.sections);
+				me.section = me.sections.find(me.selectors.section);
 				// 初始化方向
-				me.direction = me.settings.direction == "vertical" ? true : false;
+				me. ection = me.settings.direction == "vertical" ? true : false;
 				// 获取页面的数量
 				me.pagesCount = me.pagesCount();
-				me.index = (me.settings.index >= 0 && me.settings.index < pagesCount) ? me.settings.index : 0;
+				me.index = (me.settings.index >= 0 && me.settings.index < me.pagesCount) ? me.settings.index : 0;
+
+				me.canScroll = true;
 
 				//判断方向
 				if (!me.direction) {
@@ -59,13 +69,13 @@
 			//向下滑动的方法
 			next: function () {
 				var me = this;
-				if (me.index < me.pagesCount - 1) {
+				if (me.index < me.pagesCount ) {
 					me.index++;
 				} else if (me.settings.loop) {
 					me.index = 0;
 				}
 				me._scrollPage();
-			}
+			},
 			// 主要针对横屏情况，进行页面布局
 			_initLayout: function () {
 				var me = this;
@@ -77,12 +87,13 @@
 			//  实现分页的dom结构及css样式
 			_initPaging: function () {
 				var me = this,
-					pagesClass = me.selectors.page.substring(1),//截取字符串,使用substring
-					activeClass = me.selectors.active.substring(1),
-					pageHtml = "<ul class=" + pagesClass + ">";
+					pagesClass = me.selectors.page.substring(1);//截取字符串,使用substring
+				me.activeClass = me.selectors.active.substring(1);
+				var	pageHtml = "<ul class=" + pagesClass + ">";
 				for (var i = 0; i < me.pagesCount; i++){
 					pageHtml += "<li></li>";
 				}
+				pageHtml += "</ul>";
 				me.element.append(pageHtml);
 				//增加active
 				var pages = me.element.find(me.selectors.page);
@@ -109,20 +120,86 @@
 				});
 				//绑定鼠标滚动事件
 				me.element.on("mousewheel DOMMouseScroll", function (e) {
+					if (me.canScroll) {
 					var delta = e.originalEvent.wheelDelta || -e.originalEvent.detail;
 					//如果向上翻页，调用prev()，向下调用next()
 					if (delta > 0 && (me.index && !me.settings.loop || me.settings.loop)) {
 						me.prev();
-					} else if (delta < 0 && (me.index <(me.pagesCount -1)&&!me.settings.loop || me.settings.loop)) {
+					} else if (delta < 0 && (me.index < (me.pagesCount - 1) && !me.settings.loop || me.settings.loop)) {
 						me.next();
 					}
-				})
-			 }
+					}
+				});
+				//绑定键盘事件
+				if (me.settings.keybord) {
+					$(window).on("keydown", function (e) {
+						var keyCode = e.keyCode;
+						if (keyCode == 37 || keyCode == 38) {
+							me.prev();
+						} else if (keyCode == 39 || keyCode == 40) {
+							me.next();
+						}
+					});
+				}
+				//绑定resize事件
+				$(window).resize(function () {
+					var currentLength = me.switchLength(),
+						offset = me.settings.direction ? me.section.eq(me.index).offset().top : me.section.eq(me.index).offset.left;
+					if (Math.abs(offset) > currentLength / 2 && me.index < (me.pagesCount - 1)) {
+						me.index++;
+					}
+					if (me.index) {
+						me._scrollPage();
+					}
+				});
+				//绑定transitioned事件 ,用于动画结束后，执行的函数
+				me.section.on("trasitionend webkitTransitionEnd oTransitionEnd otransitionend", function () {
+					me.canScroll = true;
+					if (me.settings.callback && $.type(me.settings.callback) == "function") {
+						me.settings.callback();
+					}
+				});
+				//上面的事件10.28日再添加
+				//已添加10.28
+
+
+			},
+			// 实现滑动动画
+			_scrollPage: function () {
+				var me = this,
+					dest/*用来存放坐标值 */ = me.section.eq(me.index).position();
+				if (!dest) return;
+				me.canScroll = false;
+				// 判断浏览器是否支持transition属性
+				if (_prefix) {
+					//支持，就添加前缀
+					me.sections.css(_prefix + "transition", "all" + me.settings.duration + "ms" + me.settings.easing);
+					//每次滑动的距离。竖屏滑动，获取top值，横屏滑动，获取left值
+					var translate = me.direction ? "translateY(-" + dest.top + "px)" : "translateX(-" + dest.left + "px)";
+					//再添加transform属性
+					me.sections.css(_prefix + "transform", translate);
+				} else {
+					//不支持，就用animate()属性
+					var animateCss = me.direction ? { top: -dest.top } : { left: -dest.left };
+					me.sections.animate(animateCss, me.settings.duration, function () {
+						me.canScroll = true;
+						if (me.settings.callback && $.type(me.settings.callback) == "function") {
+							me.settings.callback();
+						}
+					});
+				}
+
+				//分页样式的调整
+				if (me.settings.pagination) {
+					me.pageItem.eq(me.index).addClass(me.activeClass).siblings("li").removeClass(me.activeClass);
+				}
+			}
+
 
 		};
 		return PageSwitch;
 	})();
-	$.fn.PageSwitch = function () {
+	$.fn.PageSwitch = function (options) {
 		return this.each(function () {
 			var me = $(this),
 				instance = me.data("PageSwitch");
@@ -143,18 +220,17 @@
 			page: ".pages",
 			active: ".active"
 		},
-		index: 0,
+		index: 1,
 		easing: "ease",
 		duration: 500,
-		loop: fasle,
+		loop: false,
 		pagination: true,
 		keybord: true,
 		direction: "vertical",
 		callback: ""
 	};
 	//在javascript内部实现初始化
-	$(function () {
-		$("[data-PageSwitch]").PageSwitch();
-	});
-
+	// $(function () {
+	// 	$("[data-PageSwitch]").PageSwitch();
+	// });
 })(jQuery);
